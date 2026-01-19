40b71e46894ec0dbdbcf823a2072a6fffffd69d1
Matt Erlandson <merlandson83@gmail.com>
2026-01-19 00:50:49 -0500

    Update site title and favicon

---
+++ src/app/flash/layout.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+    title: "Flash",
+  };
+  
+  export default function FlashLayout({
+    children,
+  }: {
+    children: React.ReactNode;
+  }) {
+    return children;
+  }
+  
